4b8da9e21e039e147122384710de461f9f0bbc6d
Daulet Almagambetov <daukabase@gmail.com>
2024-02-09 18:48:30 +0600

    move NeoQazaqKeybaord to specific file

--- Sources/KeyboardKit/Layout/Providers/NeoQazaqKeyboardLayoutProvider.swift
+++ Sources/KeyboardKit/Layout/Providers/NeoQazaqKeyboardLayoutProvider.swift
@@ -1,8 +1,19 @@
 //
-//  File.swift
-//  
+//  NeoQazaqKeyboardLayoutProvider.swift
+//
 //
 //  Created by Daulet Almagambetov on 03.02.2024.
 //
 
 import Foundation
+
+public class NeoQazaqKeyboardLayoutProvider: InputSetBasedKeyboardLayoutProvider {
+    public init() {
+        super.init(
+            alphabeticInputSet: .neoQazaq,
+            numericInputSet: .standardNumeric(currency: "₽"),
+            symbolicInputSet: .standardSymbolic(currencies: ["€", "$", "£"])
+        )
+        self.localeKey = KeyboardLocale.kazakh.id
+    }
+}
--- Sources/KeyboardKit/Layout/Providers/RussianKeyboardLayoutProvider.swift
+++ Sources/KeyboardKit/Layout/Providers/RussianKeyboardLayoutProvider.swift
@@ -7,17 +7,6 @@
 
 import Foundation
 
-public class NeoQazaqKeyboardLayoutProvider: InputSetBasedKeyboardLayoutProvider {
-    public init() {
-        super.init(
-            alphabeticInputSet: .neoQazaq,
-            numericInputSet: .standardNumeric(currency: "₽"),
-            symbolicInputSet: .standardSymbolic(currencies: ["€", "$", "£"])
-        )
-        self.localeKey = KeyboardLocale.kazakh.id
-    }
-}
-
 public class RussianKeyboardLayoutProvider: InputSetBasedKeyboardLayoutProvider {
     public init() {
         super.init(
